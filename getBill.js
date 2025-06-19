d0f5c607f90c2c827fe774fa2142d0e686f81048
rinisriranganathan <rinisri1509r@gmail.com>
2025-06-19 14:23:20 +0530

    Update getBill.js

--- getBill.js
+++ getBill.js
@@ -1,18 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
-exports.handler = async (event) => {
-  const id = event.queryStringParameters.id;
-  const filePath = path.join("/tmp", `${id}.json`);
+exports.handler = async () => {
+  const filePath = path.join("/tmp", `latest.json`);
 
   if (!fs.existsSync(filePath)) {
-    return { statusCode: 404, body: "Not found" };
+    return {
+      statusCode: 404,
+      body: "No recent bill found."
+    };
   }
 
   const data = fs.readFileSync(filePath);
   return {
     statusCode: 200,
     body: data.toString(),
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json" }
   };
 };
