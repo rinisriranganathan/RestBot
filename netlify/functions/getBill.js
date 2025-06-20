2f758152e6d42f2881252fba5ef10f42e5b93ce1
rinisriranganathan <rinisri1509r@gmail.com>
2025-06-20 10:44:34 +0530

    Create getBill.js

---
+++ netlify/functions/getBill.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async () => {
+  const filePath = path.join("/tmp", `latest.json`);
+
+  if (!fs.existsSync(filePath)) {
+    return {
+      statusCode: 404,
+      body: "No recent bill found."
+    };
+  }
+
+  const data = fs.readFileSync(filePath);
+  return {
+    statusCode: 200,
+    body: data.toString(),
+    headers: { "Content-Type": "application/json" }
+  };
+};
